ebd9178371c44fedbac32e5047fd5e0cde9d66e6
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-02-19 16:26:09 -0700

    added interview whiteboard question lol

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,3 +27,19 @@
   });
 
 })(jQuery); // End of use strict
+
+let Arr = [1, 2, 3, 4, 5]
+let targetVal = 8
+
+function sumArr(Arr, targetVal) {
+  let val = 0
+  let initialVal = Arr[val]
+  for (let i = 0; i < Arr.length; i++) {
+    let item = Arr.Find(item => initialVal - item == targetVal)
+    let newArr = [item, targetVal]
+    if (item == undefined) {
+      val += 1
+    }
+    return newArr
+  }
+}
